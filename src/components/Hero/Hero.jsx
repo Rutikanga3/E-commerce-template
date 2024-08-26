c18e4809ff0b93038381bf8903d3863ad1cde113
Rutikanga3 <rutikangadoreen@gmail.com>
2024-08-26 16:14:41 +0200

    hero section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Image1 from '../../assets/Image1.png'
+import { s } from 'vite/dist/node/types.d-aGj9QkWt'
+
+const Hero = () => {
+  return (
+    <div>
+      hero
+    </div>
+  )
+}
+
+export default Hero
