9e3ae8f796d954cd7cb70681dcc9d7e328850ecb
Rutikanga3 <rutikangadoreen@gmail.com>
2024-08-31 15:41:08 +0200

    heroo

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import Image1 from '../../assets/Image1.png'
-import { s } from 'vite/dist/node/types.d-aGj9QkWt'
+import Image2 from '../../assets/Image2.png'
+import Image3 from '../../assets/Image3.png'
+const ImageList =[
+  {
+    id:1,
+    img:Image1,
+    title:"Upto 50% off on all Men's Wear",
+    description:"lorem His Life will forever be changed dolor sit amet, consectetur adiscing elit, seddo eiusmod tempor incididunt ut labore et dolore magna aliqua.",
 
+  },
+]
 const Hero = () => {
   return (
     <div>
-      hero
+      
     </div>
   )
 }
